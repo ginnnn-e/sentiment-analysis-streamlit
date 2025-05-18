510683234770d36f69a7a0a6da4aa80f592ce105
ginnnn-e <144206861+ginnnn-e@users.noreply.github.com>
2025-05-18 14:46:34 +0000

    added confess data, display metrics

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
+st.title("emoji")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+# Read the data
+df = pd.read_csv('confess_data.csv')
+
+# Metrics
+total, confession, poll = st.columns(3)
+total.metric('Total Records', len(df))
+confession.metric('Total Confessions', len(df[df['type']=='confession']))
+poll.metric('Total Polls', len(df[df['type']=='poll']))
